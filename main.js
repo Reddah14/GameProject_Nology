5c377c45a6e18b7c4096289a6feeb52d48774ada
Salvador Martinez <cartoonrocket9@gmail.com>
2021-08-29 13:23:28 +0200

    able to log event from buttons

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert("Hello")
+alert("Hello")const lifeBarsArray = document.getElementsByClassName("progress-bar");
+console.log(lifeBarsArray);
+
+const buttonsArray = document.querySelectorAll("button");
+console.log(buttonsArray);
+
+buttonsArray.forEach(button => {
+
+  button.addEventListener("click", event => {
+    console.log(event);
+  })
+})
+

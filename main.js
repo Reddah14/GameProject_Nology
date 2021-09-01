3c702f84fa97c602b28e2eab089848614cf627f1
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-01 23:59:17 +0200

    fixing p1 attack logic

--- main.js
+++ main.js
@@ -118,23 +118,27 @@ function playerOneAttackCPU() {
     mainDiv[0].classList.add("remove-from-screen");
     finalGiffSection[1].classList.remove("remove-from-screen");
     grab_dataForEndFightGiff("victory");
+
     setTimeout( cpuTurnDecision = () => {
       // After waiting for 3 seconds, call the function below.
       logPannel.innerText = "Player 1 Wins ! 🎉🎉🎉";
       reStartGameButton.classList.remove("remove-from-screen");
       mainDiv[0].classList.add("remove-from-screen");
       finalGiffSection[1].classList.remove("remove-from-screen");
-      grab_dataForEndFightGiff("victory");
+      grab_dataForEndFightGiff("winner");
     }, 1500);
+
+
+
   } else {
     lifeBarsArray[1].style.width = readyToApplyLife;
-    logPannel.innerText = "Player 1 Attacks 🗡🗡🗡 and takes 30 life points . . . ."
-    setTimeout(function cpuTurnDecision() {
+    logPannel.innerText = `Player 1 Attacks 🗡🗡🗡 and takes ${attackDamage} life points . . . .`;
+    setTimeout( cpuTurnDecision = () => {
       // After waiting for 3 seconds, call the function below.
       console.log("from attack");
       CPUselectRandomOption();
-    }, 0500);
-  }  
+    }, 1500);
+  }
 }
 
 function playerOneSpellCPU() {
